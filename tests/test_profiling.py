7be09b111ad614da6597b3e03567ade824ca125c
Armand LEOPOLD <armand.leopold@outlook.com>
2023-11-21 16:04:59 +0100

    ci(repo): skip ci pytest step for now
    - Ajuste l’automatisation CI pour fiabiliser les builds et déploiements.

--- tests/test_profiling.py
+++ tests/test_profiling.py
@@ -1,13 +1,11 @@
-import json
-from unittest.mock import mock_open, patch
+from unittest.mock import mock_open, patch, MagicMock
+import profiling_pack.main
 
-mocked_file_data = json.dumps({
-    "type": "file",
-    "config": {"path": "data/iris.csv"},
-    "name": "test_dataset"
-})
-
-# Mocking file operations and pandas dataframe for further tests
-@patch("builtins.open", mock_open(read_data=mocked_file_data))
-def test_main_flow(mocked_file_data):
-    pass
+@patch("builtins.open", new_callable=mock_open)
+@patch("glob.glob", MagicMock(return_value=["tests/data/iris.csv"]))
+@patch("pandas.read_csv", MagicMock(return_value=MagicMock()))
+def test_main_flow(mocked_open, mocked_glob, mocked_read_csv):
+    profiling_pack.main()
+    mocked_open.assert_called_once_with("source_conf.json", "r", encoding="utf-8")
+    mocked_glob.assert_called_once_with("tests/data/iris.csv")
+    mocked_read_csv.assert_called_once()
